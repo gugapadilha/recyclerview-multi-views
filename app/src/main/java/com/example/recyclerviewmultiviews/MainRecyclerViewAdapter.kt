ae6a011fa8c00f4d7587b6916b608130fb127376
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-06-23 17:19:57 -0300

    changes is Adapter

--- app/src/main/java/com/example/recyclerviewmultiviews/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recyclerviewmultiviews/MainRecyclerViewAdapter.kt
@@ -1,4 +1,46 @@
 package com.example.recyclerviewmultiviews
 
-class MainRecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewmultiviews.databinding.ResAtorBinding
+import com.example.recyclerviewmultiviews.databinding.ResCabecalhoBinding
+
+class MainRecyclerViewAdapter : RecyclerView.Adapter<MainRecyclerViewHolder>() {
+
+    private val list = mutableListOf<MainRecyclerViewItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainRecyclerViewHolder  =
+             when(viewType){
+            TIPO_ATOR -> MainRecyclerViewHolder.AtorViewHolder(
+                ResAtorBinding.inflate(LayoutInflater.from(parent.context))
+            )
+            TIPO_CABECALHO -> MainRecyclerViewHolder.CabecalhoViewHolder(
+                ResCabecalhoBinding.inflate(LayoutInflater.from(parent.context))
+            )
+            else -> throw IllegalArgumentException("error in view")
+        }
+
+
+    override fun onBindViewHolder(holder: MainRecyclerViewHolder, position: Int) {
+
+        when(holder){
+            is MainRecyclerViewHolder.AtorViewHolder -> holder.bind(list[position] as MainRecyclerViewItem.Ator)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int = when (list[position]){
+            is MainRecyclerViewItem.Ator -> TIPO_ATOR
+            is MainRecyclerViewItem.Cabecalho -> TIPO_CABECALHO
+        }
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        const val TIPO_ATOR = 0
+        const val TIPO_CABECALHO = 1
+    }
 }
--- app/src/main/java/com/example/recyclerviewmultiviews/MainRecyclerViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewmultiviews/MainRecyclerViewHolder.kt
@@ -1,7 +1,30 @@
 package com.example.recyclerviewmultiviews
 
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import com.example.recyclerviewmultiviews.databinding.ActivityMainBinding
+import com.example.recyclerviewmultiviews.databinding.ResAtorBinding
+import com.example.recyclerviewmultiviews.databinding.ResCabecalhoBinding
 
-class MainRecyclerViewHolder(binding: ActivityMainBinding) : RecyclerView.ViewHolder(binding.root) {
+sealed class MainRecyclerViewHolder(binding: ViewBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    class CabecalhoViewHolder(
+        private val _binding : ResCabecalhoBinding
+    ) : MainRecyclerViewHolder(_binding){
+
+        fun bind(cabecalho : MainRecyclerViewItem.Cabecalho){
+            _binding.imgHeader.setImageResource(cabecalho.image)
+        }
+    }
+
+    class AtorViewHolder(
+        private val _binding : ResAtorBinding
+    ) : MainRecyclerViewHolder(_binding){
+
+        fun bind(ator: MainRecyclerViewItem.Ator){
+            _binding.tvAtor.text = ator.nome
+            _binding.tvAtorPapel.text = ator.papel
+            _binding.imgAtor.setImageResource(ator.image)
+        }
+    }
 }
